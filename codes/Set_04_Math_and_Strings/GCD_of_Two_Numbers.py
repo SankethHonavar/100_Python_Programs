0e6977a0a776b07f3f2a1496cddc60fdc9d12e11
Sanketh Honavar <sankethhonavar25@gmail.com>
2025-08-02 18:24:58 +0530

    Added a program to find the GCD of Two Numbers

---
+++ codes/Set_04_Math_and_Strings/GCD_of_Two_Numbers.py
@@ -0,0 +1,7 @@
+def compute_gcd(x,y):
+    while y:
+        x,y=y,x%y
+    return x
+num1=int(input("Enter first number: "))
+num2=int(input("Enter second number: "))
+print("GCD:", compute_gcd(num1,num2))
