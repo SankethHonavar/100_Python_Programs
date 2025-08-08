ee4422b81bd3a3e0313098b007b362197b838290
Sanketh Honavar <sankethhonavar25@gmail.com>
2025-08-08 23:23:25 +0530

    Added a program to Calculate the Power Set of a Set

---
+++ codes/Set_05_Data_Structures/Calculate_the_Power_Set_of_a_Set.py
@@ -0,0 +1,5 @@
+from itertools import chain, combinations
+def power_set(s):
+    return list(chain.from_iterable(combinations(s,r) for r in range(len(s)+1)))
+input_set=[1,2,3]
+print("Power set: ", power_set(input_set))
