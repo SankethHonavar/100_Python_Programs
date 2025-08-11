48c4f27cdbb81542c29c7f0497249a83497760cc
Sanketh Honavar <sankethhonavar25@gmail.com>
2025-08-11 22:36:47 +0530

    Added a program to Calculate the Volume of a Cylinder

---
+++ Set_06_Algorithms_and_Math/Calculate_the_Volume_of_a_Cylinder.py
@@ -0,0 +1,5 @@
+import math
+radius=float(input("Enter the radius of the cylinder: "))
+height=float(input("Enter the height of the cylinder: "))
+volume=math.pi*radius*radius*height
+print("Volume of the cylinder: ", volume)
