ab955f6f9d36962ab7c99d72bcb91fdf6ba28485
Sanketh Honavar <sankethhonavar25@gmail.com>
2025-08-28 17:35:34 +0530

    Added a program to Check if a String is a Valid Email Address

---
+++ codes/Set_09_Loops_and_Utilities/Check_if_a_String_is_a_Valid_Email_Address.py
@@ -0,0 +1,8 @@
+import re
+def is_valid_email(email):
+    return bool(re.match(r"[^@]+@[^@]+\.[^@]+", email))
+input_email=input("Enter an email address: ")
+if is_valid_email(input_email):
+    print("Valid email address")
+else:
+    print("Invalid email address")
