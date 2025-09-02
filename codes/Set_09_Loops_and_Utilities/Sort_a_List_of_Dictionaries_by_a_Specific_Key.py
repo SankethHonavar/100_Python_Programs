b55745afd9cc2aab8a96edc62b157608fe6b8ada
Sanketh Honavar <sankethhonavar25@gmail.com>
2025-09-02 23:19:49 +0530

    Added a program to Sort a List of Dictionaries by a Specific Key

---
+++ codes/Set_09_Loops_and_Utilities/Sort_a_List_of_Dictionaries_by_a_Specific_Key.py
@@ -0,0 +1,4 @@
+list_of_docs=[{'name':'John','age':30},{'name':'Jane','age':25},
+              {'name':'Bob','age':35}]
+sorted_list=sorted(list_of_docs,key=lambda x: x['age'])
+print("Sorted list of dictionaries: ", sorted_list)
