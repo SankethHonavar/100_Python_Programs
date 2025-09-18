ae658cc5953261b86c8f0e0d405b8c7efdb09c58
Sanketh Honavar <sankethhonavar25@gmail.com>
2025-09-18 23:22:36 +0530

    Added a program to Validate an IP Address

---
+++ codes/Set_07_Advanced_Logic/Validate_an_IP_Address.py
@@ -0,0 +1,14 @@
+import socket
+
+def is_valid_ip(ip):
+    try:
+        socket.inet_aton(ip)
+        return True
+    except socket.error:
+        return False
+    
+ip_address =input("Enter an IP address: ")
+if is_valid_ip(ip_address):
+    print("Valid IP address")
+else:
+    print("Invalid IP address")
