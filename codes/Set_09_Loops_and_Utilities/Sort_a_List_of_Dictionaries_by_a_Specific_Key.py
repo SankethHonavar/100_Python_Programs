ea03e12b7d37f6ccac2c4255f9dcf0eb9865babb
Sanketh Honavar <sankethhonavar25@gmail.com>
2025-09-29 23:45:17 +0530

    Delete Sort_a_List_of_Dictionaries_by_a_Specific_Key.py

--- codes/Set_09_Loops_and_Utilities/Sort_a_List_of_Dictionaries_by_a_Specific_Key.py
+++
@@ -1,4 +0,0 @@
-list_of_docs=[{'name':'John','age':30},{'name':'Jane','age':25},
-              {'name':'Bob','age':35}]
-sorted_list=sorted(list_of_docs,key=lambda x: x['age'])
-print("Sorted list of dictionaries: ", sorted_list)
